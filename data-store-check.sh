b7ef416990b2599040498abb80d91859661f2d2e
Tony Edgin <tedgin@iplantcollaborative.org>
2016-12-09 13:13:08 -0700

    added user and debug flags
    The original message verbosity is gone. The debug flag brings it back.

--- data-store-check.sh
+++ data-store-check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Usage: data-store-check.sh [HOST [PORT [CUTOFF_TIME]]]
+# Usage: data-store-check.sh [HOST [PORT [USER [CUTOFF_TIME [DEBUG]]]]]
 #
 # This script generates two reports, one for collections and one for data 
 # objects. Each report lists all collections or data objects, respectively, that
@@ -9,12 +9,18 @@
 # 
 # HOST is the domain name or IP address of the server where the PostgreSQL DBMS
 # containing the ICAT DB runs. The default value is 'localhost'.
-
+#
 # PORT is the TCP port the DBMS listens on. The default value is '5432'.
+#
+# USER is the user used to authenticate the connection to the DB. The default
+# value is 'irods'.
 #
 # CUTOFF_TIME is the upper bound of the creation time for the collections and 
 # data objects contained in the reports. The format should be the default format
 # used by `date`. The default value is today '00:00:00' in the local time zone.
+#
+# DEBUG is any value that, if present, will cause the script to generate
+# describing what it is doing. It is not present by default.
 
 set -e
 
@@ -34,14 +40,29 @@ fi
 
 if [ "$#" -ge 3 ]
 then
-  readonly CUTOFF_TIME="$3"
+  readonly USER="$3"
+else
+  readonly USER=irods
+fi
+
+if [ "$#" -ge 4 ]
+then
+  readonly CUTOFF_TIME="$4"
 else
   readonly CUTOFF_TIME=$(date -I --date=today)
 fi
 
-readonly CutoffTS=$(date --date="$CUTOFF_TIME" '+0%s')
+if [ "$#" -ge 5 ]
+then
+  readonly DEBUG=1
+fi
 
-psql --host "$HOST" --port "$PORT" ICAT icat_reader <<EOF
+
+display_problems() 
+{
+  local cutoffTS=$(date --date="$CUTOFF_TIME" '+0%s')
+
+  psql --host "$HOST" --port "$PORT" ICAT "$USER" <<EOF
 \timing on
 
 BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;
@@ -91,6 +112,7 @@ AS
   HAVING COUNT(u.object_id) != 1;
 
 \echo '1. Problem Collections Created Before $CUTOFF_TIME:'
+\echo ''
 SELECT
   coll_id = ANY(ARRAY(SELECT * FROM coll_perm_probs))  AS "Permission Issue",
   COALESCE((SELECT cu.uuid_count FROM coll_uuid_probs AS cu WHERE cu.coll_id = c.coll_id), 1) 
@@ -102,10 +124,12 @@ FROM r_coll_main AS c
 WHERE coll_id = ANY(ARRAY(SELECT * FROM coll_perm_probs UNION SELECT coll_id FROM coll_uuid_probs))
   AND coll_name LIKE '/iplant/%'
   AND coll_type != 'linkPoint'
-  AND create_ts < '$CutoffTS'
+  AND create_ts < '$cutoffTS'
 ORDER BY create_ts;
 
+\echo ''
 \echo '2. Problem Data Objects Created Before $CUTOFF_TIME:'
+\echo ''
 SELECT 
   d.data_id = ANY(ARRAY(SELECT * FROM data_perm_probs))  AS "Permission Issue",
   COALESCE((SELECT du.uuid_count FROM data_uuid_probs AS du WHERE du.data_id = d.data_id), 1) 
@@ -118,8 +142,32 @@ WHERE c.coll_name LIKE '/iplant/%'
   AND d.data_id
     = ANY(ARRAY(SELECT * FROM data_perm_probs UNION SELECT data_id FROM data_uuid_probs))
   AND d.data_repl_num = 0
-  AND d.create_ts < '$CutoffTS'
+  AND d.create_ts < '$cutoffTS'
 ORDER BY d.create_ts;
 
 ROLLBACK;
 EOF
+}
+
+
+format_results()
+{
+  while IFS= read -r
+  do
+    if [ -n "$DEBUG" ]
+    then
+      printf '%s\n' "$REPLY"
+    else
+      case "$REPLY" in
+        Timing*|Time:*|BEGIN|SELECT*|ROLLBACK)
+          ;;
+        *)
+          printf '%s\n' "$REPLY"
+          ;;
+      esac
+    fi
+  done
+}
+
+
+display_problems | format_results
