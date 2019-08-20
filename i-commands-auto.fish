4ef3e28eec6ecbffe03f9d5e674a6b3fb6d816ce
Tony Edgin <tedgin@iplantcollaborative.org>
2019-08-20 14:38:08 -0700

    i-commands-auto.fish: added ihelp completions

--- i-commands-auto.fish
+++ i-commands-auto.fish
@@ -4,6 +4,65 @@
 # https://github.com/irods/irods-legacy/blob/master/iRODS/irods_completion.bash,
 # by Bruno Bzeznik.
 
+# NOTE: USE /usr/share/fish/completions/*.fish as examples, especially scp.fish
+
+# The commands in reverse order of popularity Implement completions in order of
+# popularity, deferring the implementation of those marked with - to the end.
+#
+#   2837 ils
+#   1322 iquest
+# - 1065 iadmin
+#    997 irm
+#    948 icd
+#    748 imeta
+#    600 iput
+#    561 ichmod
+#    394 ips
+#    352 iphymv
+#    349 iget
+#    300 isysmeta
+#    289 ichksum
+#    283 imv
+#    273 ilsresc
+#    262 irepl
+#    211 irule
+#    187 imkdir
+#    126 iswitch
+#    109 itrim
+#    103 irmtrash
+#     94 iqstat
+#     60 iscan
+#     59 ierror
+#     51 ibun
+#     50 icp
+#     48 iquota
+#     39 iqdel
+#     32 iticket
+# ✓   23 ihelp
+#     22 imcoll
+#     17 irsync
+#     17 iinit
+#     11 igroupadmin
+#     10 ilocate
+#      8 igetwild
+#      5 iuserinfo
+#      5 imiscsvrinfo
+#      5 ienv
+#      3 iexit
+#      3 idbug
+#      2 iqmod
+#      2 ipwd
+#      2 ipasswd
+#      2 ilresc
+#      2 ifsck
+#      2 iexecmd
+#      2 ichown
+# -    1 izonereport
+#      1 ixmsg
+#      1 ichecksum
+#      0 ireg
+#      0 iphybun
+
 
 function __remote_path_suggestions
   if [ (count $argv) -ge 1 ]
@@ -37,8 +96,38 @@ complete --command iget \
 
 
 #
+# ihelp
+#
+
+function __ihelp_argument_suggestions
+  set cmd (commandline --cut-at-cursor --tokenize)
+  if [ (count $cmd) -eq 1 ]
+    set args \
+      iadmin ibun icd ichksum ichmod icp idbug ienv ierror iexecmd iexit ifsck iget igetwild \
+      igroupadmin ihelp iinit ilocate ils ilsresc imcoll imeta imiscsvrinfo imkdir imv ipasswd \
+      iphybun iphymv ips iput ipwd iqdel iqmod iqstat iquest iquota ireg irepl irm irmtrash irsync \
+      irule iscan isysmeta iticket itrim iuserinfo ixmsg izonereport
+    for arg in $args
+      printf '%s\n' "$arg"
+    end
+  else
+    printf ''
+  end
+end
+
+complete --command ihelp --short-option h --exclusive --description 'shows help'
+complete --command ihelp --short-option a --exclusive \
+  --description 'prints the help text for all the iCommands'
+complete --command ihelp --no-files --arguments "(__ihelp_argument_suggestions)"
+
+
+#
 # iput
 #
+# TODO extend to cover options
+# TODO make suggest appropriate arguments
+# TODO make suggest multiple local paths
+# TODO make suggest at most one remote path
 
 function __iput_after_first_arg
   set cmd (commandline --cut-at-cursor --tokenize)
@@ -65,6 +154,9 @@ complete --command irsync \
 # ibun, icd, ichksum, icp, ils, imeta, imkdir, imv, iphybun, iphymv, irm,
 # irmtrash, and itrim
 #
+# TODO extend to cover options
+# TODO make suggest appropriate arguments
+# TODO make suggest multiple arguments, if applicable
 
 for cmd in ibun icd ichksum icp ils imeta imkdir imv iphybun iphymv irm irmtrash itrim
   complete --command $cmd --no-files --arguments "(__remote_path_suggestions)"
